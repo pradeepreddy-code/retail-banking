2e96c8dd27aa77b5025a616e85f4c5ccdce574b8
pradeepreddy-code <110544037+pradeepreddy-code@users.noreply.github.com>
2025-03-25 20:58:58 -0700

    Use test profile with H2 for CI tests

--- src/test/java/xyz/subho/retail/banking/ApplicationTests.java
+++ src/test/java/xyz/subho/retail/banking/ApplicationTests.java
@@ -2,12 +2,13 @@ package xyz.subho.retail.banking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Tells Spring to use application-test.properties
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
